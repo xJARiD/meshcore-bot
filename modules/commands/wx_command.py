0820dc794b2f5d0b74d37ad2db86eae5c3c1c261
Jared Dohrman <jared@dohrman.me>
2026-06-14 10:10:56 +1000

    fix(wx): reject non-ASCII digits in day-count parsing

--- modules/commands/wx_command.py
+++ modules/commands/wx_command.py
@@ -601,7 +601,7 @@ class WxCommand(BaseCommand):
                         forecast_type = "multiday"
                         num_days = days
                         location_parts = location_parts[:-1]
-                elif last_part.isdigit():
+                elif last_part.isascii() and last_part.isdigit():
                     days = int(last_part)
                     if 2 <= days <= WX_MULTIDAY_MAX_DAYS:
                         forecast_type = "multiday"
